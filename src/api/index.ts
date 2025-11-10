1e841229914002bb4f66672bd79d93dcf5cc1e2e
“chinonso <chinonsookike@gmail.com>
2025-11-10 15:05:55 +0100

    add cors policy

--- src/api/index.ts
+++ src/api/index.ts
@@ -35,7 +35,13 @@ app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use(logger())
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: ['https://www.everliaworld.com', 'https://everliaworld.com', 'http://localhost:3000'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+  exposedHeaders: ['Content-Range', 'X-Content-Range']
+}));
 
 // Root route
 app.get("/", (req, res) => {
